65719e7b38f8865845882a59ce1fb858c8cc48a3
Baptiste P <paqueriaudbaba@gmail.com>
2022-11-21 14:24:27 +0100

    somme

---
+++ 21-11-2022/somme.py
@@ -0,0 +1,13 @@
+def somme_par_indice_pairs(nombres):
+    somme = 0
+    for i in range(len(nombres)): # Pour chaque index
+        if i%2 == 0: # S'il est pair ...
+            somme = somme + nombres[i]
+
+    return somme
+
+
+
+
+list = [1, 2, 3]
+print(somme_par_indice_pairs(list))
